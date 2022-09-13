71e273099950d2a87f8e4f80688a3083e01c6f66
Paul <paul@ovrsea.com>
2022-09-13 11:19:00 +0200

    boilerplate

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { debug, getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+import { handleError } from "./handle-error";
+
+const run = async () => {
+  try {
+    const token = getInput("github_token", { required: true });
+    debug(JSON.stringify(context, undefined, 2));
+    
+    const github = getOctokit(token);
+    debug(JSON.stringify(github));
+  } catch (error: unknown) {
+    handleError(error, setFailed);
+  }
+};
+
+void run();
